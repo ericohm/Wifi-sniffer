95e5d444eb3e200363fd57edaf675d172990471b
oalsing <oscar.alsing@gmail.com>
2015-07-14 10:12:27 +0200

    signals.txt parse cools stuff for the win

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient,
-fs 			= require('fs');
-
+	fs 			= require('fs');
+/*
 var re = /^\d+.\s([GPS_RAW_INT]+)\s(.*)$/;
 
 fs.readFile('gps.txt', 'utf8', function (err,data) {
@@ -27,4 +27,36 @@ fs.readFile('gps.txt', 'utf8', function (err,data) {
 			}
 		});
 	});
+});*/
+
+MongoClient.connect('mongodb://admin:password@ds047672.mongolab.com:47672/wifisniff', function(err, db) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log("succesfully connected");
+
+		fs.readFile('signals.txt', 'utf8', function(err, data) {
+			if(err)
+				console.log(err);
+			json = JSON.parse(data);
+			//console.log(json);
+
+			for(var key in json) {
+				//console.log(json[key]);
+				var writeObj = {
+					mac: key,
+					properties: json[key]
+				};
+
+				console.log(writeObj);
+
+				db.collection('gpsdata').insert(writeObj, function(err, recors) {
+					if(err) throw err;
+					console.log("inserted data");
+				});
+			};
+		});	
+	}
+	// weird shit happens but yolo 
+	// db.close();
 });
