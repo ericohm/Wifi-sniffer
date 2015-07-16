a24b54c4259ff0c93a7845b242bba1fa52908b1c
oalsing <oscar.alsing@gmail.com>
2015-07-16 11:11:23 +0200

    updates wifi sniffer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,
+	request		= require('request'),
 	fs 			= require('fs');
 /*
 var re = /^\d+.\s([GPS_RAW_INT]+)\s(.*)$/;
@@ -29,7 +30,7 @@ fs.readFile('gps.txt', 'utf8', function (err,data) {
 	});
 });*/
 
-MongoClient.connect('mongodb://admin:password@ds053312.mongolab.com:53312/near', function(err, db) {
+/*MongoClient.connect('mongodb://admin:password@ds053312.mongolab.com:53312/near', function(err, db) {
 	if(err) {
 		console.log(err);
 	} else {
@@ -59,4 +60,56 @@ MongoClient.connect('mongodb://admin:password@ds053312.mongolab.com:53312/near',
 	}
 	// weird shit happens but yolo 
 	// db.close();
-});
+}); */
+
+var headers = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded',
+    'x-access-token': 	'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiT3NjYXIgQWxzaW5nIiwidXNlcm5hbWUiOiJvc2Npc3BybyIsImlkIjoiNTU5YTUyNWFhMzU5Njg5M2QwNTliNDZhIiwiaWF0IjoxNDM2OTY5NTY1LCJleHAiOjE0MzcwNTU5NjV9.6fEpgIO2B0NzTRJfoV3dbwc-3dREgroPqIilJUIY_7Y'
+}
+
+fs.readFile('signals.txt', 'utf8', function(err, data) {
+			if(err)
+				console.log(err);
+			json = JSON.parse(data);
+			//console.log(json);
+
+			for(var key in json) {
+				//console.log(json[key]);
+				var writeObj = {
+					mac: key,
+					properties: json[key]
+				};
+
+				writeObj.properties.forEach(function(entry) {
+					// Configure the request
+					var options = {
+					    url: 'https://quiet-citadel-6617.herokuapp.com/api/droneentry/',
+					    method: 'POST',
+					    headers: headers,
+					    form: {
+					    	'mac': key, 
+					    	'lng': entry.Longitude,
+					    	'lat': entry.Latitude,
+					    	'signal': entry.Signal,
+					    	'time': 	entry.Time
+					    }
+					};
+
+									// Start the request
+				request(options, function (error, response, body) {
+				    if (!error && response.statusCode == 200) {
+				        // Print out the response body
+				        console.log(body);
+				    } else {
+				    	console.log(error);
+				    }
+				});
+
+				})
+
+				console.log(writeObj);
+
+				
+			};
+		});	
