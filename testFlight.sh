b6603c582f1f38fd2d27fc21ed0fc98cb1067375
ericohm <eric.c.ohman@gmail.com>
2015-08-04 11:20:03 +0200

    Update testFlight.sh

--- testFlight.sh
+++ testFlight.sh
@@ -1,47 +1,30 @@
 #!/bin/bash
+sudo echo -e '!1 SHUTDOWN' | nc localhost 2501
+sleep 1
+sudo echo -e '!1 SHUTDOWN' | nc localhost 2501
+sleep 1
+sudo echo -e '!1 SHUTDOWN' | nc localhost 2501
 echo none > /sys/class/leds/led0/trigger
 rm /home/pi/Test/*.*
 rm /home/pi/Pictures/*.*
-rm test.txt
 rm signals.txt
 rm default.p
 sudo iw phy phy0 interface add mon0 type monitor > /dev/null 2>&1 &
 sudo iw dev wlan0 del > /dev/null 2>&1 &
-
 kismet_server > /dev/null 2>&1 &
+python gpstotext.py &
 
-(while [ 1 ]; do sleep 1; done) |  mavproxy.py --master=/dev/ttyAMA0 --baudrate 57600 --aircraft Mycopter  > /dev/null 2>&1 &
-my_pid=$!
+echo "Starting up Kismet and GPS"
+sleep 30
 
-echo "Mavproxy and Kismet is starting so wait man"
+echo "Everything up and running"
 
-file_exists=0
-while [ $file_exists -lt 10 ];
-do
-        echo 1 > /sys/class/leds/led0/brightness
-	echo "Waiting for Mavproxy.."
-        echo 'status GPS_RAW_INT' > /proc/$my_pid/fd/0
-        sleep 2
-        [[ -s test.txt ]]
-        if [ $? -eq 0 ]; then
-                echo "Mavproxy has started!!"
-                let file_exists=10
-        fi;
-	echo 0 > /sys/class/leds/led0/brightness
-	sleep 2
-done
-
-
-echo "MavProxy is Online!"
 echo heartbeat > /sys/class/leds/led0/trigger
 for i in {1..10000}
 do
-        sleep 1
-        echo 'status GPS_RAW_INT' > /proc/$my_pid/fd/0
         sleep 2
-	#cat test.txt
-	#cat signals.txt
         python3 getsignals.py
+        sleep 2
         sudo echo -e '!1 SHUTDOWN' | nc localhost 2501
         kismet_server > /dev/null 2>&1 &
 done
