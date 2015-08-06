679b1088516f9dd5f450ade538ab74dc743737c2
ericohm <eric.c.ohman@gmail.com>
2015-08-06 14:26:53 +0200

    Update getsignals.py

--- getsignals.py
+++ getsignals.py
@@ -2,27 +2,33 @@
 #The kismet program running on linux saves it's log file in an xml format. This script will take this output
 #and take out the mac addresses and store the signal strength from each device.
 
+#Big edit on august 6th, we decided to try this out on the computer. All code which involves rasperry pi specific modules
+#and code, i.e. the picamera is commented out. 
 import xml.etree.ElementTree as ET
 import pickle
 import glob, os
 import time
 import json
-import picamera
+#import picamera
 import datetime
 
 
 #Nameing the file we save to as default
 iteration = "default"
 
+#Another threed is run where we contionously save the coordinates to a file called gps.p
+#The script which saves the coordinates is called gpstotext.py
 def getGPS():
 	cords = pickle.load(open("gps.p","rb"))
 	latitude = cords[0]
 	longitude = cords[1]
 	return latitude,longitude
 
+
+#This is where we save the information from the wifi togheter with gps coordinates.
 def newPoint(lat,lon):
     '''files variable stores all the netxml files in the folder in a list, it then sorts it and takes out the most recent one'''
-    files = glob.glob(os.path.abspath("/home/pi/Test/*.netxml"))
+    files = glob.glob(os.path.abspath("/home/ericsson/*.netxml"))
     files.sort()
     fileX = files[-1]
 
@@ -47,7 +53,6 @@ def newPoint(lat,lon):
             if bssid in signals:
                 if signals[bssid]["Entries"][-1]["Time"] != last_time:
                     signals[bssid]["Entries"].append(temp)
-                    print(temp["Signal":])
             else:
             	signals[bssid] = {"Entries":[]}
             	signals[bssid]["Entries"].append(temp)
@@ -60,14 +65,15 @@ def newPoint(lat,lon):
     
     '''After this script is run for the last time, the script approximate.py is run to add our best estimates'''
 
-def takePicture(lat,lon):
-    '''Function that takes in x,y (latitude longitude) and a name and takes a picture, and saves the picture to that name.'''
-    ts = time.time()
-    st = "DATE: "+datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    with picamera.PiCamera() as camera:
-        name = "CORDS: "+str(lat) +","+ str(lon)+st+".jpg"
-        camera.capture(name)
+#Commented out everything because raspberry
+#def takePicture(lat,lon):
+#    '''Function that takes in x,y (latitude longitude) and a name and takes a picture, and saves the picture to that name.'''
+#    ts = time.time()
+#    st = "DATE: "+datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+#    with picamera.PiCamera() as camera:
+#        name = "CORDS: "+str(lat) +","+ str(lon)+st+".jpg"
+#        camera.capture(name)
 
 lat,lon = getGPS()
 newPoint(lat,lon)
-takePicture(lat,lon)
+#takePicture(lat,lon)
