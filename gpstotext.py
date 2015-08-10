4bde1b5156262c61a8d9b8d4b1598a73bf482abf
ericohm <eric.c.ohman@gmail.com>
2015-08-10 09:40:13 +0200

    Update gpstotext.py

--- gpstotext.py
+++ gpstotext.py
@@ -9,6 +9,7 @@ from time import *
 import time
 import threading
 import pickle
+import time
  
 gpsd = None #seting the global variable
  
@@ -29,15 +30,17 @@ if __name__ == '__main__':
   gpsp = GpsPoller() # create the thread
   lat = 0
   lon = 0
+  start_time = time.time()
   try:
     gpsp.start() # start it up
     
     while True:
+      theTime = int(round(time.time()-start_time,0))
       if math.isnan(gpsd.fix.latitude) or math.isnan(gpsd.fix.longitude):
-        cords = [lat,lon]
+        cords = [lat,lon,theTime]
       else:
        lat,lon = gpsd.fix.latitude,gpsd.fix.longitude
-      cords = [lat,lon]
+      cords = [lat,lon,theTime]
       pickle.dump(cords,open("gps.p","wb"))
       time.sleep(1) #set to whatever
  
