17c01be1778e38b964956b35653a96978722fe70
ericohm <eric.c.ohman@gmail.com>
2015-08-10 09:44:15 +0200

    Update read_fft.py

--- read_fft.py
+++ read_fft.py
@@ -13,40 +13,57 @@ class countvar:
 	signals = {"Entries":[]}
 
 def getGPS():
-    cords = pickle.load(open("gps.p","rb"))
-    latitude = cords[0]
-    longitude = cords[1]
-    return latitude,longitude
+	cords = pickle.load(open("gps.p","rb"))
+	latitude = cords[0]
+	longitude = cords[1]
+	theTime = cords[2]
+	return latitude,longitude,theTime
 
-def getTime():
-    stamp = pickle.load(open("time.p",'rb'))
-    return stamp
+#def getTime():
+#    stamp = pickle.load(open("time.p",'rb'))
+#    return stamp
 
 def calculate_gsm(lat,lon,time):
+	print("how")
 	f = scipy.fromfile(open("gsm_power.txt"), dtype=scipy.int8)
+	print("long")
 	temp = f[countvar.count_interval:len(f)]
 	count1 = 0
 	count2 = 0
-	bit = 0
+	person = 0
+	last = False
+	numberOfPersons = 0
 	for j in temp:
 		if j > threshold:
 			count1 = count1 + j
 			count2 = count2+1
-			bit = 1
-	if bit==1:
+			if last == True:
+				person = person +1
+			else:
+				person=1
+			last = True
+		else:
+			person = 0
+			last = False
+
+		if person >39:
+			numberOfPersons = numberOfPersons +1
+			person = 0
+
+	if numberOfPersons>0:
 		average = count1/max(count2,1)
-		print(average)
-		temp = {"Signal":average, "Longitude":lon,"Latitude":lat,"VideoTime":time}
+
+		temp = {"Signal":average, "Longitude":lon,"Latitude":lat,"VideoTime":time,"Persons":numberOfPersons}
+		print(temp)
 		countvar.signals["Entries"].append(temp)
 		#print(countvar.signals)
 
 
 	countvar.count_interval = len(f)
 
-for i in range(0,1000):
-	lat,lon=getGPS()
-	time =getTime()
-	calculate_gsm(lat,lon,time)
+for i in range(0,1000000):
+	lat,lon,thetime=getGPS()
+	calculate_gsm(lat,lon,thetime)
 	'''print(countvar.count_interval)'''
 	time.sleep(1)
 	pickle.dump(countvar.signals,open(iteration+".p","wb"))
